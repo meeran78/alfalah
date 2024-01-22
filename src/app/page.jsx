cb5a21a10d778dd85abef8797168dee1c0cf11c0
meeran78 <meeran78@gmail.com>
2024-01-22 17:10:02 -0500

    ZG

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,18 @@
 import Image from "next/image";
+import Video from "next-video";
+import styles from "./page.module.css";
+
+import img1 from "./../../public/masjidfront1.jpg";
+import img2 from "./../../public/masjidfront2.jpg";
+import img3 from "./../../public/masjidfrontnight.jpg";
 //Homepage
 export default function Home() {
   return (
-    <div>Homepage</div>
-  )
+    // <div><Video src="./videos/bg2.mp4" loop autoPlay muted/></div>
+    <>      
+      <div className={styles.sliderContainer}>
+        <Image src={img1} alt="" />
+      </div>
+    </>
+  );
 }
--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,5 +1,10 @@
-import Link from "next/link";
+"use client";
+
+import { useState } from "react";
 import NavLink from "./navLink/navLink";
+import styles from "./Links.module.css";
+import Image from "next/image";
+
 const links = [
   {
     title: "Home",
@@ -29,19 +34,48 @@ const links = [
     title: "Contact Us",
     path: "/contact",
   },
-  {
-    title: "Admin",
-    path: "/admin",
-  },
 ];
 
-const Links = () => {
+// TEMPORARY
+const session = true;
+const isAdmin = true;
+
+const Links = ({ session }) => {
+  const [open, setOpen] = useState(false);
+  
   return (
-    <div>
-      {links.map((link => (
-        // <Link href={link.path} key={link.title}>{link.title}</Link>
-        <NavLink item={link} />
-      )))}
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          // <Link href={link.path} key={link.title}>{link.title}</Link>
+          <NavLink item={link} key={link.title}/>
+        ))}
+        {session ? (
+          <>
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            {/* <form action={handleLogout}> */}
+            <button className={styles.logout}>Logout</button>
+            {/* </form> */}
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      <Image 
+        className={styles.menuButton}
+        src="/darkMenuIcon.png"
+        alt=""
+        width={30}
+        height={30}
+        onClick={() => setOpen((prev) => !prev)}
+      />
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link}  key={link.title}/>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
